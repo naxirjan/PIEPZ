1850bb57b025c5e2eade24ed01420ab6e2e355b3
Naxir Jan <nazir.ahmed13626@gmail.com>
2023-08-27 09:22:26 -0700

    test

---
+++ resources/views/vendors/products/ajax/ajax-get-bulk-edit-products.blade.php
@@ -0,0 +1,50 @@
+@php $iCount = 0; $sStatus = ""; @endphp
+@foreach($aProducts as $aProduct)
+@php
+$sIsFeatured = ($aProduct->is_featured == 1 ? "checked=checked" : "");
+$sIsApproved = ($aProduct->is_approved == 1 ? "checked=checked" : "");
+$sStatus = ($aProduct->status == 1 ? "checked=checked" : "");
+@endphp
+<tr>
+    <td><input name="SelectedProduct_{{$iCount}}" class="form-check-input cb-element" type="checkbox" value="{{base64_encode($aProduct->id)}}" /> </td>
+    <td style="width:20%"><input name="name_{{$iCount}}" type="text" value="{{$aProduct->name}}-{{$aProduct->id}}" class="form-control"></td>
+    <td style="width:15%"><input name="type_{{$iCount}}" type="text" value="{{$aProduct->type}}" class="form-control"></td>
+    <td style="width:30%">
+    
+        <select name="category_id_{{$iCount}}" class="select2 form-select  category_id" data-allow-clear="true">
+            <option value="">-</option>
+            @foreach($objCategories as $objCategory)
+            <option value="{{$objCategory->id}}" {{($aProduct->category_id == $objCategory->id ? "Selected=Selected" : "")}}>{{$objCategory->name}}</option>
+            @endforeach
+        </select>
+        <script>
+        $(".category_id").select2({});
+        </script>
+    </td>
+
+    <td style="width:15%"><input name="price_{{$iCount}}" type="number" value="{{$aProduct->price}}" class="form-control"></td>
+    <td style="width:20%"><input name="short_description_{{$iCount}}" type="text" value="{{$aProduct->short_description}}" class="form-control"></td>
+    <td><label class="switch switch-sm pe-4">
+            <input name="is_featured_{{$iCount}}" type="checkbox" class="switch-input" {{$sIsFeatured}}>
+            <span class="switch-toggle-slider">
+                <span class="switch-on"></span>
+                <span class="switch-off"></span>
+            </span>
+        </label></td>
+    <td><label class="switch switch-sm pe-4">
+            <input name="is_approved_{{$iCount}}" type="checkbox" class="switch-input" {{$sIsApproved}}>
+            <span class="switch-toggle-slider">
+                <span class="switch-on"></span>
+                <span class="switch-off"></span>
+            </span>
+        </label></td>
+    <td><label class="switch switch-sm pe-4">
+            <input name="status_{{$iCount}}" type="checkbox" class="switch-input" {{$sStatus}}>
+            <span class="switch-toggle-slider">
+                <span class="switch-on"></span>
+                <span class="switch-off"></span>
+            </span>
+        </label></td>
+</tr>
+@php $iCount++; @endphp
+@endforeach
---
+++ resources/views/vendors/products/product-bulk-edit.blade.php
@@ -0,0 +1,211 @@
+@extends('layouts/layoutMaster')
+@section('title', 'DataTables - Tables')
+
+@section('vendor-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/quill/typography.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/quill/katex.css')}}" />
+
+
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/select2/select2.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/tagify/tagify.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/bootstrap-select/bootstrap-select.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/typeahead-js/typeahead.css')}}" />
+
+@endsection
+
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/select2/select2.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/bootstrap-select/bootstrap-select.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/bloodhound/bloodhound.js')}}"></script>
+
+<script>
+$( document ).ready(function() {
+
+    //Get All Products
+    $("#GetProducts").load('ajax-get-bulk-edit-products');
+
+    //Check All
+    $("#checkAll").change(function () {
+    $(".cb-element").prop('checked', $(this).prop("checked"));
+});
+
+
+    //Check Single
+    $(document).on('click', '.cb-element',function()
+     {
+        if($('.cb-element:checked').length == $('.cb-element').length)
+        {
+            $('#checkAll').prop('checked',true);
+        }
+        else{
+            $('#checkAll').prop('checked',false);
+        }
+    });
+
+
+    //Search By Category
+    $("#idBtnSearch").click(function ()
+     {
+        var iCategoryId = $("#idCategory").val();
+        var iStatusId = $("#idStatus").val();
+        var iVendor = $("#idVendor").val();
+
+        if( (iCategoryId == "" || iCategoryId == null) &&  (iStatusId == "" || iStatusId == null) && (iVendor == "" || iVendor == null))
+        {
+            alert("Please Select Any Filter !...");
+            return false;
+        }
+
+        $.ajax({
+                type:'POST',
+                url:'ajax-get-bulk-edit-products-by-filters',
+                data:{
+                    _token: "{{ csrf_token() }}",
+
+                    iCategoryId:iCategoryId,
+                    iStatusId:iStatusId,
+                    iVendor:iVendor
+                },
+                success: function( sResponse )
+                {
+                    $("#GetProducts").html(sResponse);
+                }
+            });
+//
+     });
+
+
+    // Reset Filter
+    $("#idBtnResetFilter").click(function () {
+
+        $("#idCategory").val('').trigger('change');
+        $("#idStatus").val('').trigger('change');
+        $("#idVendor").val('').trigger('change');
+
+        //Get All Products
+        $("#GetProducts").load('ajax-get-bulk-edit-products');
+    });
+
+
+});
+</script>
+@endsection
+
+@section('page-script')
+
+<script src="{{asset('assets/js/forms-selects.js')}}"></script>
+<script src="{{asset('assets/js/forms-typeahead.js')}}"></script>
+@endsection
+
+
+@section('content')
+<div class="container-xxl flex-grow-1 container-p-y">
+<h4 class="fw-bold py-3 mb-4">
+   <span class="text-muted fw-light">Admin /</span> Bulk Update Products
+</h4>
+
+
+
+    <div class="row">
+  <!-- Vendor -->
+  <div class="col-xl-4 col-md-4 col-6 mb-4">
+    <div class="card">
+      <div class="card-body">
+         <label for="idVendor" class="form-label">Vendor</label>
+            <select id="idVendor" class="select2 form-select form-select-lg" data-allow-clear="true">
+             <option value="">-</option>
+              <option value="-1">All</option>
+             @foreach($objVendors as $objVendor)
+                <option value="{{$objVendor->id}}">{{$objVendor->first_name}} {{$objVendor->last_name}}</option>
+             @endforeach
+            </select>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Category -->
+  <div class="col-xl-4 col-md-4 col-6 mb-4">
+    <div class="card">
+      <div class="card-body">
+          <label for="idCategory" class="form-label">Category</label>
+            <select id="idCategory" class="select2 form-select form-select-lg" data-allow-clear="true">
+              <option value="">-</option>
+              <option value="-1">All</option>
+                @foreach($objCategories as $objCategory)
+                    <option value="{{$objCategory->id}}">{{$objCategory->name}}</option>
+                @endforeach
+            </select>
+
+      </div>
+    </div>
+  </div>
+
+<!-- Status-->
+    <div class="col-xl-4 col-md-4 col-6 mb-4">
+    <div class="card">
+      <div class="card-body">
+             <label for="idStatus" class="form-label">Status</label>
+            <select id="idStatus" class="select2 form-select form-select-lg" data-allow-clear="true">
+              <option value="">-</option>
+              <option value="-1">All</option>
+              <option value="0">In Active</option>
+              <option value="1">Active</option>
+            </select>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+    <div class="row">
+        <div class="col-xl-4 col-md-4 col-6 mb-4 text-center">
+		<button id="idBtnSearch" class="btn btn-success btn-sm me-5 waves-effect waves-light" type="button">Search <i class="ti ti-search ti-sm"></i></button></div>
+        <div class="col-xl-4 col-md-4 col-6 mb-4"></div>
+        <div class="col-xl-4 col-md-4 col-6 mb-4 text-center">
+		<button id="idBtnResetFilter" class="btn btn-info btn-sm me-5 waves-effect waves-light" type="button">
+                Reset Filters<i class="ti ti-refresh ti-sm"></i></button>
+	</div>
+    </div>
+
+
+<!-- Scrollable -->
+<div class="card" style="padding:20px;">
+    @if (session('msg'))
+    <p class="alert alert-success text-center">
+        {{ session('msg') }}
+    </p>
+    @endif
+    <div class="row">
+   <div class="table-responsive">
+      <form action="bulk-update-procducts-process" method="post" >
+       <table class="table">
+         <thead>
+            <tr>
+                <th><input class="form-check-input" type="checkbox" id="checkAll" /></th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Category</th>
+                <th>&nbsp;Price&nbsp;</th>
+                <th>Short Description</th>
+                <th>Is Featured</th>
+                <th>Is Approved</th>
+                <th>Status</th>
+            </tr>
+         </thead>
+         <tbody id ="GetProducts"></tbody>
+      </table>
+       <br />
+        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+       <button name="submit" class="btn btn-success btn-lg waves-effect waves-light" type="submit" value="submit">Update All</button>
+       </form>
+   </div>
+    </div>
+</div>
+<!--/ Scrollable -->
+
+
+</div>
+@endsection
